0894b929ba782667f40372e2e52a50799e1eca11
Veg mahajan <madhavmahajan912@gmail.com>
2025-11-20 23:18:37 +0530

    Aggregate_functions_queries

---
+++ Aggregate_functions.sql
@@ -0,0 +1,35 @@
+create table personal(
+id int not null unique,
+name varchar(20) not null,
+percentage int not null,
+age int not null check(age>=18),
+gender varchar(6) not null,
+phone varchar(10) not null unique,
+city varchar(15) not null default 'Delhi');
+insert into personal (id, name, percentage, age, gender, phone, city)
+values
+(1, "Ram Kumar", 45, 19, "M", 4022155, "Agra"), 
+(2, "Sarita Kumari", 55, 21,"F",4034421, "Delhi"), 
+(3, "Salman Khan", 62, 20, "M",4056221, "Agra"), 
+(4, "Juhi Chawla", 47, 18, "F",4089821, "Bhopal"), 
+(5, "Anil Kapoor", 74, 22, "M",4025221, "Agra"), 
+(6, "John Abraham", 64, 21, "M",4033776, "Delhi"),
+(7, "Shahdi kapoor", 52, 20, "M", 4022784, "Agra");
+
+## Aggregate Functions
+
+Select count(name) from personal;
+
+Select count(*) from personal;
+
+Select max(percentage) from personal;
+
+Select min(percentage) from personal;
+
+Select sum(percentage) from personal;
+
+Select avg(percentage) from personal;
+
+Select count(distinct city) from personal;
+
+	
